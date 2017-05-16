9c9a23e15a4c712ce6bc8070e26a3551cf3c29e8
destino92 <destinodello@gmail.com>
2017-05-16 23:56:02 +0300

    feat: defined the table component to display a list of users with loans and redefined App.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import loanData from '../data.js';
+import classNames from 'classnames';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -24,8 +26,43 @@ class App extends Component {
 
 class Table extends Component {
   render() {
+    const {list} = this.props;
     return(
-      <div>This is suppose to be a table</div>
+      <div className="table">
+        <div className="table-header">
+          <span style={{ width: '40%' }}>
+            Name
+          </span>
+          <span style={{ width: '30%' }}>
+            ID
+          </span>
+          <span style={{ width: '15%' }}>
+            Phone Number
+          </span>
+          <span style={{ width: '15%' }}>
+            Loan Balance
+          </span>
+        </div>
+        {list.map(item =>
+          <div
+            key={item.object_id}
+            className="table-row"
+          >
+            <span style={{ width: '40%' }}>
+              <a href="#">{item.full_name}</a>
+            </span>
+            <span style={{ width: '30%' }}>
+              {item.national_id}
+            </span>
+            <span style={{ width: '15%' }}>
+              {item.mobile_number}
+            </span>
+            <span style={{ width: '15%' }}>
+              {item.loan_balance}
+            </span>
+          </div>
+        )}
+      </div>
     )
   }
 }
--- src/data.js
+++ src/data.js
@@ -2,20 +2,23 @@ const loans = [
   {
     national_id: '1234567899',
     mobile_number: '0712345699',
-    name: 'Lionel Messi',
-    loan_balance: '500000'
+    full_name: 'Lionel Messi',
+    loan_balance: '500000',
+    object_id: 234
   },
   {
     national_id: '1234567888',
     mobile_number: '0712345688',
-    name: 'Cristiano Ronaldo',
-    loan_balance: '350000'
+    full_name: 'Cristiano Ronaldo',
+    loan_balance: '350000',
+    object_id: 456
   },
   {
     national_id: '1234567877',
     mobile_number: '0712345677',
-    name: 'Didier Drogba',
-    loan_balance: '750960'
+    full_name: 'Didier Drogba',
+    loan_balance: '750960',
+    object_id: 345
   }
 ]
 
