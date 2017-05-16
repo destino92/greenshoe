71532f7315d94fdae9dda2df9c7cd60dd210a254
destino92 <destinodello@gmail.com>
2017-05-16 22:36:10 +0300

    feat: initialized app components

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        Let's get it started!!!
+      </div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(
-  <div>Let's get it started!</div>,
+  <App/>,
   document.getElementById('root')
 );
