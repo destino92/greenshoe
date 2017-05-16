1f6e8e4bf994dd1fc9e18ef9c3aeee85bb21ab7b
destino92 <destinodello@gmail.com>
2017-05-16 22:50:43 +0300

    feat: added state to App component and created Table components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loans: null,
+    }
+  };
+
+  render() {
+    const {loans} = this.state;
+    return(
+      <div className="page">
+        <Table
+          list={loans}
+        />
+      </div>
+    );
+  }
+}
+
+class Table extends Component {
   render() {
     return(
-      <div>
-        Let's get it started!!!
-      </div>
+      <div>This is suppose to be a table</div>
     )
   }
 }
